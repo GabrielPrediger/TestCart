f9cbfd8d99361083d394b5d2c0d692f1b7be542d
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2021-11-21 23:56:59 -0300

    master

---
+++ src/Pages/Main/App.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [ item, setItem ] = useState([])
+  const [ totalValue, setTotalValue] = useState([])
+  const total = totalValue[0]?.value + totalValue[1]?.value
+
+
+  useEffect(() => {
+   fetch('./abaixo.json')
+   .then(response => response.json())
+   .then(data => {
+    setItem(data.items) 
+    setTotalValue(data.totalizers)
+   })
+  }, [])
+
+  return (
+    <div className="main">
+      <h1>Meu carrinho</h1>
+      <div className="divisor"/>
+      {item.map(itens => 
+      <div className="card">
+        <img src={itens.imageUrl} alt="Candy Images"/>
+        <div>
+          <p className="item-name">{itens.name}</p>
+          <p className="item-price1">R$ {itens.listPrice/100}</p>
+          <p className="item-price2">R$ {itens.sellingPrice/100}</p>
+        </div>
+      </div>
+      )}
+      <div className="divisor"/>
+      <div className="total-container">
+        <h1>Total</h1>
+        <h1>R$ {total / 100}</h1>
+      </div>
+      <div className="container-free">
+        { total / 100 >  10 && (
+        <div className="free">Parabéns, sua compra tem frete grátis !</div>
+        )}
+      </div>
+      <div className="divisor"/>
+      <div className="container-free">
+        <div className="button">Finalizar compra</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
